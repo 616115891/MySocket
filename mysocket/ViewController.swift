2a3b4e94ce897307e16bdae8c8f3e4fe36829e88
YJY <ye@localhost.com>
2017-04-24 15:50:08 +0800

    Initial Commit

---
+++ mysocket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mysocket
+//
+//  Created by Ye on 4/24/17.
+//  Copyright © 2017 Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
